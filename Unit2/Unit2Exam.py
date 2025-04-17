c07d6c2aff14feb392d2527ada6f665b44314acc
phantomized <boon.atticus@gmail.com>
2025-04-17 11:14:55 -0700

    implemented more custom pets

--- Unit2/Unit2Exam.py
+++ Unit2/Unit2Exam.py
@@ -1,16 +1,18 @@
 import random
 class VirtualPet:
-    def __init__(self,name=str,price=float,condition=100.00,happiness=100,hunger=0,energy=100,days_starving=0):
+    def __init__(self,gender=str,name=str,price=float,condition=random.randint(75,125),happiness=random.randint(75,125),hunger=random.randint(75,125),energy=random.randint(75,125),days_starving=0):
         """Initializes class VirtualPet
 
         Args:
             name (str, optional): name of pet. Defaults to str.
             price (float, optional): cprice of pet. Defaults to float.
             condition (float, optional): condition of pet. Defaults to 100.00.
+            gender (str, optional): gender of pet. Defaults to str.
             happiness (int, optional): happiness of pet. Defaults to 100.
             hunger (int, optional): hunger of pet. Defaults to 100.
             energy (int, optional): energy of pet. Defaults to 100.
         """
+        self.gender = gender
         self.name = name
         self.condition = condition
         self.happiness = happiness
@@ -25,7 +27,7 @@ class VirtualPet:
             str: name, species, condition, happiness, hunger, energy, price
         """
         self.check_stats()
-        return f"Name: {self.name}\nSpecies: {self.species}\nCondition: {self.condition_status()}%\nHappiness: {self.status(self.happiness,1)}\nHunger: {self.status(self.hunger,2)}\nEnergy: {self.status(self.energy,3)}\nPrice: ${self.price_status():0.2f}"
+        return f"Name: {self.name}\nSpecies: {self.species}\nGender: {self.gender}\nCondition: {self.condition_status()}%\nHappiness: {self.status(self.happiness,1)}{self.happiness}\nHunger: {self.status(self.hunger,2)}{self.hunger}\nEnergy: {self.status(self.energy,3)}{self.energy}\nPrice: ${self.price_status():0.2f}"
     def __add__(self,other):
         """Overrides adding for VirtualPet class
 
@@ -108,17 +110,23 @@ class VirtualPet:
                 else:
                     return energy_list[6]
     def check_stats(self):
-        if self.happiness < 0:
+        if self.happiness <= 0:
+            self.hunger += self.happiness*self.days_starving
+            self.energy += self.happiness*self.days_starving
             self.happiness = 0
-        if self.hunger < 0:
+        if self.hunger <= 0:
+            self.happiness += self.hunger*self.days_starving
+            self.energy += self.hunger*self.days_starving
             self.hunger = 0
-        if self.energy < 0:
+        if self.energy <= 0:
+            self.happiness += self.energy*self.days_starving
+            self.hunger += self.energy*self.days_starving
             self.energy = 0
         if self.happiness > 150:
             self.happiness = 150
-        if self.hunger < 150:
+        if self.hunger > 150:
             self.hunger = 150
-        if self.energy < 150:
+        if self.energy > 150:
             self.energy = 150
     def play(self):
         play_input = int(input(f"How hard do you want to play with {self.name}?\n1. Gentle\n2. Normal\n3. Hard\n(1/2/3) "))
@@ -128,12 +136,20 @@ class VirtualPet:
         food_list = [0,random.randint(1,5),random.randint(5,10),random.randint(10,15)]
         eat_input = int(input(f"How much do you want to feed {self.name}?\n1. Nothing\n2. A little: ${food_list[0]}\n3. Normal: ${food_list[1]}\n4. A lot: ${food_list[2]}\n(1/2/3/4) "))
         if eat_input != 1:
-            self.days_starving = 0
-            self.hunger += random.randint(eat_input,food_list[eat_input-1])
-            self.happiness += eat_input*random.randint(0,food_list[eat_input-1])//2
+            if self.species == "cat":
+                if random.randint(1,2+eat_input) == 1:
+                    print(f"{self.name} turns their nose up at the food and won't eat it")
+                    self.days_starving += 1
+                    self.happiness -= self.days_starving*5
+                    self.energy -= self.days_starving*5
+                else:
+                    self.days_starving = 0
+                    self.hunger += random.randint(eat_input,food_list[eat_input-1])
+                    self.happiness += eat_input*random.randint(0,food_list[eat_input-1])//2
         else:
             self.days_starving += 1
-            self.happiness -= self.days_starving*self.hunger//2
+            self.happiness -= self.days_starving*5
+            self.energy -= self.days_starving*5
         if self.hunger < 25 or 75 < self.hunger:
             if self.hunger < 25:
                 self.energy -= random.randint(5,10)
@@ -141,23 +157,84 @@ class VirtualPet:
                 self.energy -= random.randint(0,5)
         else:
             self.energy += random.randint(1*eat_input,5*eat_input)
-    
+    def change_name(self):
+        name_choice = input(f"What do you want to change {self.name}'s name to? ")
+        self.name = name_choice
 class Dog(VirtualPet):
     species = "dog"
+    happiness = random.randint(75,125)
+    hunger = random.randint(30,75)
+    energy = random.randint(hunger,100)
 class Cat(VirtualPet):
     species = "cat"
+    happiness = random.randint(75,125)
+    hunger = random.randint(50,100)
+    energy = random.randint(30,hunger)
 class Fish(VirtualPet):
     species = "fish"
+    happiness = random.randint(50,100)
+    hunger = random.randint(75,125)
+    energy = random.randint(50,hunger)
+    def play(self):
+        print("Warning: It might not be a good idea to play with a fish")
+        continue_play = input(f"Play with {self.name}? (yes/no) ")
+        if continue_play == "yes":
+            play_input = int(input(f"How hard do you want to play with {self.name}?\n1. Gentle\n2. Normal\n3. Hard\n(1/2/3) "))
+            if random.randint(1,8-play_input) == 1:
+                print(f"You accidentally squished {self.name} to death!")
+            else:
+                self.happiness += random.randint(1*play_input,10*play_input)
+                self.energy -= random.randint(1*play_input,5*play_input)
 class Hamster(VirtualPet):
-    species = "hampster"
+    species = "hamster"
 
+def gender():
+    random_gender = random.randint(1,2)
+    if random_gender == 1:
+        return "male"
+    else:
+        return "female"
+def name(gender):
+    male_pet_names = [
+    "Max", "Charlie", "Cooper", "Buddy", "Rocky", "Teddy", "Milo", "Leo", "Jack", "Toby",
+    "Bear", "Duke", "Oliver", "Jake", "Zeus", "Louie", "Finn", "Oscar", "Sam", "Murphy",
+    "Bailey", "Chester", "Rex", "Winston", "Simba", "Gus", "Bentley", "Rusty", "Bruno", "Jasper",
+    "Marley", "Moose", "Baxter", "Apollo", "Boomer", "Tank", "Hank", "Shadow", "Ace", "Blue",
+    "Frankie", "Ziggy", "Oreo", "Scooter", "Archie", "Rocco", "Diesel", "Casper", "Chico", "Peanut",
+    "Remy", "Buzz", "Lucky", "Ranger", "Thor", "Roscoe", "Harley", "Cash", "Loki", "George",
+    "Zeke", "Hunter", "Bruce", "Sparky", "Maverick", "Bandit", "Ralph", "Scooby", "Alfie", "Yogi",
+    "Einstein", "Cody", "Frodo", "Porter", "Shadow", "Rusty", "Benny", "Tyson", "Nico", "Zorro",
+    "Taco", "Toby", "Monty", "Chief", "Blaze", "Django", "Rebel", "Vinnie", "Watson", "Rowdy",
+    "Jax", "Django", "Clifford", "Pablo", "Nugget", "Skip", "Mickey", "Romeo", "King", "Major"
+]
+    female_pet_names = [
+    "Bella", "Luna", "Lucy", "Daisy", "Lola", "Sadie", "Maggie", "Chloe", "Sophie", "Lily",
+    "Zoe", "Coco", "Roxy", "Gracie", "Molly", "Rosie", "Lulu", "Abby", "Stella", "Penny",
+    "Mia", "Nala", "Ellie", "Ginger", "Ruby", "Olive", "Willow", "Hazel", "Harley", "Nova",
+    "Annie", "Millie", "Honey", "Missy", "Bonnie", "Princess", "Izzy", "Angel", "Trixie", "Remy",
+    "Pixie", "Peaches", "Sasha", "Fiona", "Layla", "Dixie", "Maddie", "Mocha", "Poppy", "Phoebe",
+    "Sky", "Winnie", "Bambi", "Pumpkin", "Pearl", "Snowball", "Freya", "Minnie", "Precious", "Elsa",
+    "Cupcake", "Nina", "Toffee", "Snickers", "Dotty", "Opal", "Tinkerbell", "Queenie", "Velvet", "Mochi",
+    "Sugar", "Buttercup", "Gigi", "Kiki", "Lacey", "Jewel", "Summer", "Blossom", "Cherry", "Goldie",
+    "Marley", "Hope", "Taffy", "Sassy", "Fluffy", "Cinnamon", "Bubbles", "Cookie", "Twinkle", "Snowflake",
+    "Ivy", "Raven", "Stormy", "Peach", "Amber", "Callie", "Delilah", "Gypsy", "Noodle", "Kona"
+]
+    if gender == "male":
+        return male_pet_names[random.randint(0,100)]
+    else:
+        return female_pet_names[random.randint(0,100)]
+def species():
+    pet_list = [Dog,Cat,Fish,Hamster]
+    return pet_list[random.randint(0,3)]
 def main():
-    pet1 = Dog("Joe",250)
-    print(pet1)
-    pet1.play()
-    pet1.eat()
-    print(pet1)
-    #pet2 = Fish("Billy",50)
-    #print(pet2)
+    price_min = 10
+    price_max = 100
+    initial_price = random.randint(price_min,price_max)
+    random_gender = gender()
+    dog_pet1 = species()(random_gender,name(random_gender),initial_price)
+    print(dog_pet1)
+    cat_pet2 = Cat()
+    fish_pet3 = Fish()
+    hamster_pet4 = Hamster()
 if __name__ == main():
     main()
