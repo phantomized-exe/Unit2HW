469bb18664392cc974d6b099361650c9bb0e248e
Phann Boon <boon.atticus@gmail.com>
2025-04-18 04:11:09 +0000

    added id generation for pets

--- Unit2/Unit2Exam.py
+++ Unit2/Unit2Exam.py
@@ -110,6 +110,8 @@ class VirtualPet:
                 else:
                     return energy_list[6]
     def check_stats(self):
+        """resets the stats if they go too high or low
+        """
         if self.happiness <= 0:
             self.hunger += self.happiness*self.days_starving
             self.energy += self.happiness*self.days_starving
@@ -129,10 +131,15 @@ class VirtualPet:
         if self.energy > 150:
             self.energy = 150
     def play(self):
+        """play increases happiness and decreases energy and food
+        """
         play_input = int(input(f"How hard do you want to play with {self.name}?\n1. Gentle\n2. Normal\n3. Hard\n(1/2/3) "))
         self.happiness += random.randint(1*play_input,10*play_input)
         self.energy -= random.randint(1*play_input,5*play_input)
+        self.hunger -= 1*play_input
     def eat(self):
+        """eat increases all stats but costs money. you can also starve your pet
+        """
         food_list = [0,random.randint(1,5),random.randint(5,10),random.randint(10,15)]
         eat_input = int(input(f"How much do you want to feed {self.name}?\n1. Nothing\n2. A little: ${food_list[0]}\n3. Normal: ${food_list[1]}\n4. A lot: ${food_list[2]}\n(1/2/3/4) "))
         if eat_input != 1:
@@ -145,6 +152,7 @@ class VirtualPet:
                 else:
                     self.days_starving = 0
                     self.hunger += random.randint(eat_input,food_list[eat_input-1])
+                    self.energy += random.randint(1,food_list[eat_input-1]//1.5)
                     self.happiness += eat_input*random.randint(0,food_list[eat_input-1])//2
         else:
             self.days_starving += 1
@@ -158,24 +166,43 @@ class VirtualPet:
         else:
             self.energy += random.randint(1*eat_input,5*eat_input)
     def change_name(self):
+        """changes name of pet
+        """
         name_choice = input(f"What do you want to change {self.name}'s name to? ")
         self.name = name_choice
 class Dog(VirtualPet):
+    """class Dog inherits from VirtualPet
+
+    Args:
+        VirtualPet (class): main class for virtual pet
+    """
     species = "dog"
     happiness = random.randint(75,125)
     hunger = random.randint(30,75)
     energy = random.randint(hunger,100)
 class Cat(VirtualPet):
+    """class Cat inherits from VirtualPet
+
+    Args:
+        VirtualPet (class): main class for virtual pet
+    """
     species = "cat"
     happiness = random.randint(75,125)
     hunger = random.randint(50,100)
     energy = random.randint(30,hunger)
 class Fish(VirtualPet):
+    """class Fish inherits from VirtualPet
+
+    Args:
+        VirtualPet (class): main class for virtual pet
+    """
     species = "fish"
     happiness = random.randint(50,100)
     hunger = random.randint(75,125)
     energy = random.randint(50,hunger)
     def play(self):
+        """overrides play method because fish dont like to be played with
+        """
         print("Warning: It might not be a good idea to play with a fish")
         continue_play = input(f"Play with {self.name}? (yes/no) ")
         if continue_play == "yes":
@@ -186,6 +213,11 @@ class Fish(VirtualPet):
                 self.happiness += random.randint(1*play_input,10*play_input)
                 self.energy -= random.randint(1*play_input,5*play_input)
 class Hamster(VirtualPet):
+    """class Hamster inherits from VirtualPet
+
+    Args:
+        VirtualPet (class): main class for virtual pet
+    """
     species = "hamster"
     happiness = random.randint(65,100)
     hunger = random.randint(85,100)
@@ -198,6 +230,7 @@ def gender():
     else:
         return "female"
 def name(gender):
+    global name_list
     male_pet_names = [
     "Max", "Charlie", "Cooper", "Buddy", "Rocky", "Teddy", "Milo", "Leo", "Jack", "Toby",
     "Bear", "Duke", "Oliver", "Jake", "Zeus", "Louie", "Finn", "Oscar", "Sam", "Murphy",
@@ -223,14 +256,19 @@ def name(gender):
     "Ivy", "Raven", "Stormy", "Peach", "Amber", "Callie", "Delilah", "Gypsy", "Noodle", "Kona"
 ]
     if gender == "male":
-        return male_pet_names[random.randint(0,99)]
+        male_name = random.randint(0,99)
+        name_list.append(male_pet_names[male_name])
+        return male_pet_names[male_name]
     else:
-        return female_pet_names[random.randint(0,99)]
+        female_name = random.randint(0,99)
+        name_list.append(female_pet_names[female_name])
+        return female_pet_names[female_name]
 def species():
     pet_list = [Dog,Cat,Fish,Hamster]
     return pet_list[random.randint(0,3)]
 def generate_pet_id():
     global code_list
+    global name_list
     digits_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
     check_duplicates = True
     while check_duplicates:
@@ -244,17 +282,23 @@ def generate_pet_id():
         else:
             continue
 def display_pet_id():
-    pass
+    global code_list
+    global name_list
+    for i in range(len(name_list)):
+        print(f"Pet ID: {code_list[i]}, Pet Name: {name_list[i]}")
 
 def main():
     global code_list
-    code_list = {}
+    global name_list
+    code_list = []
+    name_list = []
     price_min = 10
     price_max = 100
     initial_price = random.randint(price_min,price_max)
     random_gender = gender()
+    random_name = name(random_gender)
     generate_pet_id()
-    code_list[0] = species()(random_gender,name(random_gender),initial_price)
+    code_list[0] = species()(random_gender,random_name,initial_price)
     print(code_list[0])
 if __name__ == main():
     main()
